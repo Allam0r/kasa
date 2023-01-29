2cbd32fa3eb895c2a7279ddf40d1bf697e6ff289
Allam0r <florian.wiart05@gmail.com>
2023-01-29 08:47:51 +0100

    Fix after mentorat

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+// import LodgingPage from "./pages/LodgingPage/Lodging";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import RoutesPath from "./routes/RoutesPath";
+
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <RoutesPath/>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
