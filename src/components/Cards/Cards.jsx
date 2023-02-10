01c6c3cc8e2c465d934207ea1eea0e755473bbd8
Allam0r <florian.wiart05@gmail.com>
2023-02-10 10:38:19 +0100

    Minor adjust

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,8 +6,10 @@ const Cards = ({ cover, title }) => {
   return (
     <article className="card_logement">
       <img className="card-img" src={cover} alt="Location Kasa" />
-      <div className="card-logement_text">
-        <p className="card-logement_title">{title}</p>
+      <div className="filter-card">
+        <div className="card-logement_text">
+          <p className="card-logement_title">{title}</p>
+        </div>
       </div>
     </article>
   );
