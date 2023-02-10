4bc141348f9ce94cff6831753ab778d1ec72b895
Allam0r <florian.wiart05@gmail.com>
2023-02-10 11:11:16 +0100

    Add route to lodging

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-// import LodgingPage from "./pages/LodgingPage/Lodging";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import RoutesPath from "./routes/RoutesPath";
@@ -11,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <RoutesPath/>
+      <RoutesPath />
       <Footer />
     </BrowserRouter>
   );
--- src/routes/RoutesPath.jsx
+++ src/routes/RoutesPath.jsx
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 import Error from "../pages/Error/Error";
+import Lodging from "../pages/LodgingPage/Lodging";
 
 const RoutesPath = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="*" element={<Error/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/lodging/:id" element={<Lodging />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 };
