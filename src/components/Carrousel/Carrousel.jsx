4a7b099517e5146a74f0d5d1965d955161db9678
Allam0r <florian.wiart05@gmail.com>
2023-02-10 21:41:18 +0100

    Add comments

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,7 +1,60 @@
+// Importation de la bibliothèque React et du hook useState de React
 import React from "react";
+import { useState } from "react";
 
-const Carrousel = () => {
-  return <div></div>;
-};
+// Importation des images svg de la flèche gauche et de la flèche droite
+import LeftArrow from "./images/Vector left.svg";
+import RightArrow from "./images/Vector right.svg";
 
-export default Carrousel;
+// Déclaration et export par défaut du composant Slider
+export default function Slider({ imageSlider }) {
+  // Déclaration de l'état currentIndex avec le hook useState et initialisation à 0
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Fonction pour afficher la prochaine image du diaporama
+  const nextSlide = () => {
+    // Incrémentation de currentIndex
+    setCurrentIndex(currentIndex + 1);
+    // Si currentIndex atteint la dernière image, on retourne à la première image
+    if (currentIndex === imageSlider.length - 1) setCurrentIndex(0);
+  };
+
+  // Fonction pour afficher la précédente image du diaporama
+  const prevSlide = () => {
+    // Décrémentation de currentIndex
+    setCurrentIndex(currentIndex - 1);
+    // Si currentIndex atteint la première image, on retourne à la dernière image
+    if (currentIndex === 0) setCurrentIndex(imageSlider.length - 1);
+  };
+
+  return (
+    <section
+      style={{ backgroundImage: `url(${imageSlider[currentIndex]})` }}
+      className="carrousel"
+    >
+      {/* Affichage des flèches de navigation et du compteur de slides uniquement si le diaporama contient plus d'une image */}
+      {imageSlider.length > 1 && (
+        <>
+          {/* Image de la flèche gauche, cliquable pour afficher la précédente image */}
+          <img
+            className="left-arrow"
+            src={LeftArrow}
+            alt="Previous Slide"
+            onClick={prevSlide}
+          />
+          {/* Image de la flèche droite, cliquable pour afficher la prochaine image */}
+          <img
+            className="right-arrow"
+            src={RightArrow}
+            alt="Next Slide"
+            onClick={nextSlide}
+          />
+          {/* Affichage du numéro de la diapositive courante et du nombre total de diapositives */}
+          <p className="slideCount">
+            {currentIndex + 1} / {imageSlider.length}{" "}
+          </p>
+        </>
+      )}
+    </section>
+  );
+}
--- src/pages/LodgingPage/Lodging.jsx
+++ src/pages/LodgingPage/Lodging.jsx
@@ -1,66 +1,84 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import LodgingInfo from "../../data/lodging.json";
+import Slider from "../../components/Carrousel/Carrousel";
 import Collapse from "../../components/Collapse/Collapse";
 import greyStar from "../../assets/images/greystar.svg";
 import redStar from "../../assets/images/redstar.svg";
 
+// Composant Lodging qui gère l'affichage des informations sur un logement en particulier
 const Lodging = () => {
+  // Déclaration de l'état qui contiendra les images du slider
+  const [imageSlider, setImageSlider] = useState([]);
+
+  // Récupération de l'id du logement actuel à partir de l'URL
   const idLodging = useParams("id").id;
+  // Filtrage des données du logement actuel à partir de l'id
   const dataCurrentLodging = LodgingInfo.filter(
     (data) => data.id === idLodging
   );
 
+  // useEffect pour mettre à jour l'état de l'imageSlider lorsque l'ID de l'hébergement change
   useEffect(() => {
+    // Récupération des informations pour l'hébergement actuel en utilisant son ID
     const dataCurrentLodging = LodgingInfo.filter(
       (data) => data.id === idLodging
     );
-    // setImageSlider(dataCurrentLodging[0].pictures);
+    // Mise à jour de l'état de l'imageSlider avec les images pour l'hébergement actuel
+    setImageSlider(dataCurrentLodging[0].pictures);
   }, [idLodging]);
 
+  // Découpage du nom du propriétaire pour n'afficher que les prénoms
   const name = dataCurrentLodging[0].host.name.split(" ");
+  // Note attribuée à l'hébergement
   const rating = dataCurrentLodging[0].rating;
+  // Description de l'hébergement
   const description = dataCurrentLodging[0].description;
+  // Équipements inclus dans l'hébergement
   const equipments = dataCurrentLodging[0].equipments;
 
   return (
-    <div className="lodging">
-      <div className="lodging-content">
-        <div className="lodging-content_infos">
-          <h1> {dataCurrentLodging[0].title} </h1>
-          <p> {dataCurrentLodging[0].location} </p>
-          <div>
-            {dataCurrentLodging[0].tags.map((tag, index) => {
-              return <button key={index}>{tag}</button>;
-            })}
+    <>
+      {/* Affichage du Slider */}
+      <Slider imageSlider={imageSlider} />
+      <div className="lodging">
+        <div className="lodging-content">
+          <div className="lodging-content_infos">
+            <h1> {dataCurrentLodging[0].title} </h1>
+            <p> {dataCurrentLodging[0].location} </p>
+            <div>
+              {dataCurrentLodging[0].tags.map((tag, index) => {
+                return <button key={index}>{tag}</button>;
+              })}
+            </div>
           </div>
+          <div className="lodging-content-host">
+            <span>{name[0]}</span>
+            <span>{name[1]}</span>
+          </div>
+          <img
+            src={dataCurrentLodging[0].host.picture}
+            alt="Propriétaire du logement"
+          />
         </div>
-        <div className="lodging-content-host">
-          <span>{name[0]}</span>
-          <span>{name[1]}</span>
+        <div className="lodging-content-rating">
+          {[...Array(5)].map((star, index) => {
+            const ratingValue = index + 1;
+            return (
+              <img
+                key={index}
+                src={ratingValue <= rating ? redStar : greyStar}
+                alt="Star"
+              />
+            );
+          })}
         </div>
-        <img
-          src={dataCurrentLodging[0].host.picture}
-          alt="Propriétaire du logement"
-        />
+        <div className="lodging-content-collapse">
+          <Collapse title="Description" tag="text" content={description} />
+          <Collapse title="Équipements" tag="list" content={equipments} />
+        </div>
       </div>
-      <div className="lodging-content-rating">
-        {[...Array(5)].map((star, index) => {
-          const ratingValue = index + 1;
-          return (
-            <img
-              key={index}
-              src={ratingValue <= rating ? redStar : greyStar}
-              alt="Star"
-            />
-          );
-        })}
-      </div>
-      <div className="lodging-content-collapse">
-        <Collapse title="Description" tag="text" content={description} />
-        <Collapse title="Équipements" tag="list" content={equipments} />
-      </div>
-    </div>
+    </>
   );
 };
 
