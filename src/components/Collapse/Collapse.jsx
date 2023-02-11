ce5b231b8e8b41546e840d26eeb4caa1c3da2a8e
Allam0r <florian.wiart05@gmail.com>
2023-02-11 07:45:28 +0100

    Minor fix

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -5,8 +5,10 @@ import down from "./images/VectorDOWN.svg";
 import "./Collapse.css";
 
 const Collapse = ({ title, content, tag }) => {
+  // Définition de l'état isCollapsed pour savoir si le contenu est développé ou réduit
   const [isCollapsed, setIsCollapsed] = useState(true);
 
+  // Fonction pour basculer l'état isCollapsed lorsque l'utilisateur clique sur le bouton
   const toggleCollapse = () => {
     setIsCollapsed(!isCollapsed);
   };
@@ -22,7 +24,7 @@ const Collapse = ({ title, content, tag }) => {
           alt={`Toggle ${title}`}
         />
       </h2>
-
+      {/* Contenu développé ou réduit en fonction de l'état isCollapsed */}
       {!isCollapsed && tag === "text" && (
         <p className="collapse-desc">{content}</p>
       )}
